5cef2e0436aebec105b40289c59efc37e171f35c
Pawel Suchocki <pawel.suchocki212@gmail.com>
2018-05-08 20:06:58 +0200

    upgrade

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-var span = $('span');
+	var span = $('span');
+	    paragraphs = $('p');
+	    button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
+
     span.each(function(index, element) {
-        if(index % 2 == 0) {
+        if(index % 2 === 0) {
             $(element).css('color', 'red');
         };
     });
 
-    var paragraphs = $('p');
     paragraphs.each(function(index, element) {
-        var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
         $(element).append(button);
     });
 
